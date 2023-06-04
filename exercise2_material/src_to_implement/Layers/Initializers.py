be84eba36f378b3d0861a6d38d75b1f4ac6672e8
pillai-amal <mail.vortextv@gmail.com>
2023-06-04 17:30:15 +0200

    msg

---
+++ exercise2_material/src_to_implement/Layers/Initializers.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Constant:
+    def __init__(self, inital_weight = 0.1):
+        self.inital_weight = inital_weight
+
+    def initialize(weights_shape, fan_in, fan_out):
+        return np.full((fan_in, fan_out), weights_shape)
+
+class UniformRandom:
+    def initialize(weights_shape, fan_in=0, fan_out=1):
+        return np.random.uniform(low=fan_in, high=fan_out, size=weights_shape)
+
+class Xavier:
+    def initialize(weights_shape, fan_in, fan_out):
+        limit = np.sqrt(6 / (fan_in + fan_out))
+        return np.random.uniform(-limit, limit, size=weights_shape)
+
+class He:
+    def initialize(weights_shape, fan_in, fan_out):
+        limit = np.sqrt(2 / fan_in)
+        return np.random.randn(*weights_shape) * limit
