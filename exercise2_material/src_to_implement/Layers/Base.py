173af4c7b5312e91b7d3d81bdd439cf6a47c2d0b
pillai-amal <mail.vortextv@gmail.com>
2023-06-05 10:06:32 +0200

    alle

---
+++ exercise2_material/src_to_implement/Layers/Base.py
@@ -0,0 +1,5 @@
+class BaseLayer:
+    def __init__(self):
+        self.trainable = False
+        # add other members here if needed, like a default weights parameter
+
--- exercise2_material/src_to_implement/Optimization/Optimizers.py
+++ exercise2_material/src_to_implement/Optimization/Optimizers.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+class Sgd:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        updated_weights = weight_tensor - self.learning_rate * gradient_tensor
+        return updated_weights
+
 class SgdWithMomentum:
     def __init__(self,learning_rate, momentum_rate):
         self.learning_rate = learning_rate
