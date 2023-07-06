09871d6279b0fc2e91236941d4ad1542ae36c623
pillai-amal <mail.vortextv@gmail.com>
2023-07-06 12:34:46 +0200

    allgemain code

--- Regularization und Recurrent/src_to_implement/Layers/RNN.py
+++ Regularization und Recurrent/src_to_implement/Layers/RNN.py
@@ -1,25 +1,23 @@
 #imports
 import numpy as np
-import copy
+    import copy
 from Layers import Base
 from Layers.FullyConnected import FullyConnected
 from Layers.TanH import TanH
 
 #Elman RRN Implementation
+#hidden_size = Dimension des Verstekten Zustands
+#input_size = Dimension des Eingangsvektor
 class RNN(Base.BaseLayer):
-                                                           #hidden_size = Dimension des Verstekten Zustands
-    def __init__(self,input_size,hidden_size,output_size): #input_size = Dimension des Eingangsvektor
+    def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.trainable = True
         #Aufgabe 2.2/1
+        self._memorize = False
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         #Aufgabe 2.2/2
-        self._memorize = False
-        #Zusatzliche Parametern fur RRN
-        self.h_t = None #Erste versteckten Zustand existiert nicht. 
-        self.h_mem = []
         self.FC_h = FullyConnected(hidden_size + input_size, hidden_size)
         self.FC_y = FullyConnected(hidden_size, output_size)
         self.gradient_weights_n = np.zeros((self.hidden_size + self.input_size + 1, self.hidden_size))
@@ -28,12 +26,15 @@ class RNN(Base.BaseLayer):
         self.weights = self.FC_h.weights
         self.tan_h = TanH()
         self.bptt = 0
+        #Ersten versteckten Zustand existiert nicht. 
+        self.h_t = None
         self.prev_h_t = None
         self.batch_size = None
         self.optimizer = None
+        self.h_mem = []
 
-    #Aufgabe 2.2/3 Eine Funktion, die Eingangsvektor fur weiteren Layer zuruckgibt
-    def forward(self,input_tensor):
+#Aufgabe 2.2/3 Eine Funktion, die Eingangsvektor fur weiteren Layer zuruckgibt
+    def forward(self, input_tensor):
         self.batch_size = input_tensor.shape[0]
         #Konditional Schleife, Entschiedet ob versteckten Zustand als Null betrachten oder von vorherigen Iteration benutzten.
         if self._memorize:
@@ -46,42 +47,56 @@ class RNN(Base.BaseLayer):
 
         y_t = np.zeros((self.batch_size, self.output_size))
 
+#Aufgabe 2.2/4, Eine Funktion, die Error Tensors zuruckgibt
         for b in range(self.batch_size):
             hidden_ax = self.h_t[b][np.newaxis, :]
             input_ax = input_tensor[b][np.newaxis, :]
+            # x̃_t:
             input_new = np.concatenate((hidden_ax, input_ax), axis = 1)
 
             self.h_mem.append(input_new)
 
             w_t = self.FC_h.forward(input_new)
             input_new = np.concatenate((np.expand_dims(self.h_t[b], 0), np.expand_dims(input_tensor[b], 0)), axis=1)
-            self.h_t[b+1] = TanH().forward(w_t) 
+            self.h_t[b+1] = TanH().forward(w_t) # h t = tanh (x̃ t · W h )
             y_t[b] = (self.FC_y.forward(self.h_t[b + 1][np.newaxis, :]))
         
         self.prev_h_t = self.h_t[-1]
         self.input_tensor = input_tensor
 
         return y_t
-    #Aufgabe 2.2/4, Eine Funktion, die Error Tensors zuruckgibt
+
     def backward(self, error_tensor):
+
         self.out_error = np.zeros((self.batch_size, self.input_size))
+
         self.gradient_weights_y = np.zeros((self.hidden_size + 1, self.output_size))
         self.gradient_weights_h = np.zeros((self.hidden_size+self.input_size+1, self.hidden_size))
+
         count = 0
+
         grad_tanh = 1-self.h_t[1::] ** 2
         hidden_error = np.zeros((1, self.hidden_size))
+
+        # 1: for t from 1 to T do:
+        # 2:    Run RNN for one step, computing h_t and y_t
+        # 3:    if t mod k_1 == 0:
+        # 4:        Run BPTT from t down to t-k_2
         
         for b in reversed(range(self.batch_size)):
             yh_error = self.FC_y.backward(error_tensor[b][np.newaxis, :])
             self.FC_y.input_tensor = np.hstack((self.h_t[b+1], 1))[np.newaxis, :]
+
             grad_yh = hidden_error + yh_error
             grad_hidden = grad_tanh[b]*grad_yh
             xh_error = self.FC_h.backward(grad_hidden)
             hidden_error = xh_error[:, 0:self.hidden_size]
             x_error = xh_error[:, self.hidden_size:(self.hidden_size + self.input_size + 1)]
             self.out_error[b] = x_error
+
             con = np.hstack((self.h_t[b], self.input_tensor[b],1))
             self.FC_h.input_tensor = con[np.newaxis, :]
+            # print(count, "", self.bptt)
             if count <= self.bptt:
                 self.weights_y = self.FC_y.weights
                 self.weights_h = self.FC_h.weights
@@ -132,5 +147,3 @@ class RNN(Base.BaseLayer):
     @gradient_weights.setter
     def gradient_weights(self, gradient_weights):
         self.FC_y.gradient_weights = gradient_weights
-
-                                                             
